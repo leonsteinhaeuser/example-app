6e302f5e318fe45e0960ffd8040ea39c80143630
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2023-04-26 00:13:09 +0200

    feat(lib/log): added log middleware

--- lib/log/loggable.go
+++ lib/log/loggable.go
@@ -1,5 +1,11 @@
 package log
 
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5/middleware"
+)
+
 type Logger interface {
 	SetModule(module string) Logger
 	Trace() Field
@@ -14,4 +20,17 @@ type Field interface {
 	Field(key string, value interface{}) Field
 	Log(message string)
 	Logf(format string, args ...interface{})
+}
+
+func LoggerMiddleware(log Logger) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log.Debug().
+				Field("method", r.Method).
+				Field("path", r.URL.Path).
+				Field("request_id", middleware.GetReqID(r.Context())).
+				Log("incoming request")
+			next.ServeHTTP(w, r)
+		})
+	}
 }
