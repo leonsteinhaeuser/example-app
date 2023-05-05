060e6ed42265f3cc832c2362300e81887e763260
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2023-05-05 22:06:02 +0200

    chore(articlecomment-service): added message events

--- articlecomment-service/accessobjects/articlecomment.go
+++ articlecomment-service/accessobjects/articlecomment.go
@@ -6,43 +6,92 @@ import (
 	"github.com/leonsteinhaeuser/example-app/lib"
 	"github.com/leonsteinhaeuser/example-app/lib/db"
 	"github.com/leonsteinhaeuser/example-app/lib/log"
+	"github.com/leonsteinhaeuser/example-app/lib/pubsub"
 )
 
 type ArticleComment struct {
-	db  db.Repository
-	log log.Logger
+	pubsubClient pubsub.Client
+	db           db.Repository
+	log          log.Logger
 }
 
-func NewArticleComment(db db.Repository, log log.Logger) *ArticleComment {
+func NewArticleComment(db db.Repository, log log.Logger, pubsub pubsub.Client) *ArticleComment {
 	return &ArticleComment{
-		db:  db,
-		log: log,
+		db:           db,
+		log:          log,
+		pubsubClient: pubsub.SetTopic("articlecomment"),
 	}
 }
 
 func (a *ArticleComment) Create(ctx context.Context, article *lib.ArticleComment) error {
-	return a.db.Create(ctx, article)
+	err := a.db.Create(ctx, article)
+	if err != nil {
+		return err
+	}
+	err = a.pubsubClient.Publish(&pubsub.DefaultEvent{
+		ResourceID: article.ID,
+		ActionType: pubsub.ActionTypeCreate,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (a *ArticleComment) Update(ctx context.Context, article *lib.ArticleComment) error {
-	return a.db.Update(ctx, article, db.Selector{
+	err := a.db.Update(ctx, article, db.Selector{
 		Field: "id",
 		Value: article.ID,
 	})
+	if err != nil {
+		return err
+	}
+	err = a.pubsubClient.Publish(&pubsub.DefaultEvent{
+		ResourceID: article.ID,
+		ActionType: pubsub.ActionTypeUpdate,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (a *ArticleComment) Delete(ctx context.Context, article *lib.ArticleComment) error {
-	return a.db.Delete(ctx, article, db.Selector{
+	err := a.db.Delete(ctx, article, db.Selector{
 		Field: "id",
 		Value: article.ID,
 	})
+	if err != nil {
+		return err
+	}
+	err = a.pubsubClient.Publish(&pubsub.DefaultEvent{
+		ResourceID: article.ID,
+		ActionType: pubsub.ActionTypeDelete,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (a *ArticleComment) DeleteByArticleID(ctx context.Context, article *lib.ArticleComment) error {
-	return a.db.Delete(ctx, article, db.Selector{
+	err := a.db.Delete(ctx, article, db.Selector{
 		Field: "article_id",
 		Value: article.ArticleID,
 	})
+	if err != nil {
+		return err
+	}
+	err = a.pubsubClient.Publish(&pubsub.DefaultEvent{
+		ActionType: pubsub.ActionTypeDelete,
+		AdditionalData: map[string]interface{}{
+			"article_id": article.ArticleID,
+		},
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (a *ArticleComment) Get(ctx context.Context, id string) (*lib.ArticleComment, error) {
