cbfea06b55e7388e5b38abdd450c0a24003e2184
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2023-05-24 21:38:14 +0200

    refactor(number-service): use http default server and generic router

--- number-service/main.go
+++ number-service/main.go
@@ -6,17 +6,28 @@ import (
 	"net/http"
 
 	"github.com/leonsteinhaeuser/example-app/internal"
+	"github.com/leonsteinhaeuser/example-app/internal/env"
+	"github.com/leonsteinhaeuser/example-app/internal/server"
+)
+
+var (
+	httpServer = server.NewDefaultServer(env.GetStringEnvOrDefault("LISTEN_ADDRESS", ":1111"))
+	httpRouter = server.NewGenericRouter()
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	httpRouter.AddEndpoint("GET", "/", func(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(internal.NumberResponse{
 			Number: rand.Int63(),
 		})
 	})
-	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	httpRouter.AddEndpoint("GET", "/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("ok"))
 	})
-	http.ListenAndServe(":1111", nil)
+	httpServer.AddRouter(httpRouter)
+	err := httpServer.Start()
+	if err != nil {
+		panic(err)
+	}
 }
