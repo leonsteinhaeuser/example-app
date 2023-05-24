baf50eeb8e96682b37ff6a2a8f0e35280e8bba92
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2023-05-24 22:28:06 +0200

    feat: implemented GetIntEnvOrDefault function

--- internal/env/env.go
+++ internal/env/env.go
@@ -1,10 +1,28 @@
 package env
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
+// GetStringEnvOrDefault returns the value of the environment variable key.
+// If the environment variable is not set, it returns def.
 func GetStringEnvOrDefault(key, def string) string {
 	if val := os.Getenv(key); val != "" {
 		return val
+	}
+	return def
+}
+
+// GetIntEnvOrDefault returns the value of the environment variable key as an int.
+// If the environment variable is not set or cannot be parsed as an int, it returns def.
+func GetIntEnvOrDefault(key string, def int) int {
+	if val := os.Getenv(key); val != "" {
+		ival, err := strconv.Atoi(val)
+		if err != nil {
+			return def
+		}
+		return ival
 	}
 	return def
 }
--- internal/env/env_test.go
+++ internal/env/env_test.go
@@ -51,3 +51,58 @@ func TestGetStringEnvOrDefault(t *testing.T) {
 		})
 	}
 }
+
+func TestGetIntEnvOrDefault(t *testing.T) {
+	type args struct {
+		key string
+		def int
+	}
+	type testEnv struct {
+		val string
+	}
+	tests := []struct {
+		name string
+		args args
+		env  *testEnv
+		want int
+	}{
+		{
+			name: "env not set",
+			args: args{
+				key: "TEST",
+				def: 10,
+			},
+			env:  nil,
+			want: 10,
+		},
+		{
+			name: "env set and valid",
+			args: args{
+				key: "TEST",
+				def: 0,
+			},
+			env:  &testEnv{val: "1"},
+			want: 1,
+		},
+		{
+			name: "env set and not valid",
+			args: args{
+				key: "TEST",
+				def: 10,
+			},
+			env:  &testEnv{val: "asd"},
+			want: 10,
+		},
+	}
+	for _, tt := range tests {
+		if tt.env != nil {
+			os.Setenv(tt.args.key, tt.env.val)
+			defer os.Unsetenv(tt.args.key)
+		}
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetIntEnvOrDefault(tt.args.key, tt.args.def); got != tt.want {
+				t.Errorf("GetIntEnvOrDefault() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
