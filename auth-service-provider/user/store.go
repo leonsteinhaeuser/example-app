39f9bd21eb80dd97349bc8f1e091900aa4202dc0
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2023-06-04 21:12:37 +0200

    feat: implemented user storage

--- auth-service-provider/user/store.go
+++ auth-service-provider/user/store.go
@@ -2,18 +2,19 @@ package user
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/leonsteinhaeuser/example-app/internal/db"
 )
 
-// UserStore represents the storage interface to the user store
-type UserStore interface {
+// Store represents the storage interface to the user store
+type Store interface {
 	GetUserByID(context.Context, string) (*User, error)
 	GetUserByLogin(context.Context, string) (*User, error)
 }
 
 var (
-	_ UserStore = (*UserStoreDB)(nil)
+	_ Store = (*UserStoreDB)(nil)
 )
 
 type UserStoreDB struct {
@@ -21,9 +22,19 @@ type UserStoreDB struct {
 }
 
 func (u UserStoreDB) GetUserByID(ctx context.Context, id string) (*User, error) {
-	return nil, nil
+	user := &User{}
+	err := u.DB.Find(user).Where("id", id).Commit(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get user by id %q: %w", id, err)
+	}
+	return user, nil
 }
 
 func (u UserStoreDB) GetUserByLogin(ctx context.Context, login string) (*User, error) {
-	return nil, nil
+	user := &User{}
+	err := u.DB.Find(user).Where("username", login).Or("email", login).Commit(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get user by login %q: %w", login, err)
+	}
+	return user, nil
 }
