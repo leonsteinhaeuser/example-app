a4aa62d6f394ab43909455025e5a34a0ac8e38be
Leon Steinhäuser <42437185+leonsteinhaeuser@users.noreply.github.com>
2023-06-04 20:05:26 +0200

    feat(db): implemented sql transaction handling (#7)

--- internal/db/database.go
+++ internal/db/database.go
@@ -2,30 +2,25 @@ package db
 
 import "context"
 
-// Options represents a field and a value to select a specific row in the database.
-// The underlying database implementation should use the field and value to create a WHERE clause.
-//
-// Example:
-//
-//		selector := Options{
-//			Field: "id",
-//			Value: 1,
-//		}
-//
-//	 The above selector should create a WHERE clause like this:
-//		WHERE id = 1
-type Options struct {
-	Field string
-	Value any
-	Limit *int
+type TX interface {
+	// Where adds a where clause to the query.
+	Where(field string, value any) TX
+	// Or adds an or clause to the query.
+	Or(field string, value any) TX
+	// Not adds a not clause to the query.
+	Not(field string, value any) TX
+	// Limit adds a limit clause to the query.
+	Limit(limit int) TX
+	// Commit executes the query.
+	Commit(ctx context.Context) error
 }
 
 // Repository represents an interface between the application and the database.
 type Repository interface {
 	Create(ctx context.Context, data any) error
-	Find(ctx context.Context, data any, selectors ...Options) error
-	Update(ctx context.Context, data any, selectors ...Options) error
-	Delete(ctx context.Context, data any, selectors ...Options) error
+	Find(data any) TX
+	Update(data any) TX
+	Delete(data any) TX
 	Raw(ctx context.Context, query string, args ...any) error
 	Migrate(ctx context.Context, model any) error
 	Close(context.Context) error
