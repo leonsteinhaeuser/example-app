bd2a55ea7c7e19219c2e48fa6470fbe5067350ee
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2023-06-05 23:37:00 +0200

    feat: implemented refresh token store

---
+++ auth-service-provider/provider/refresh_token_store.go
@@ -0,0 +1,47 @@
+package provider
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+
+	"github.com/leonsteinhaeuser/example-app/internal/keystore"
+	"github.com/zitadel/oidc/v2/example/server/storage"
+)
+
+type RefreshTokenStore struct {
+	kv keystore.KeyStore
+}
+
+func prefixRefreshToken(key string) string {
+	return "rtk_" + key
+}
+
+func (s *RefreshTokenStore) Get(ctx context.Context, key string) (*storage.Token, error) {
+	token := &storage.Token{}
+	tk, err := s.kv.Get(ctx, prefixRefreshToken(key))
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(tk, token)
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
+}
+
+func (s *RefreshTokenStore) Put(ctx context.Context, key string, token *storage.Token) error {
+	tk, err := json.Marshal(token)
+	if err != nil {
+		return err
+	}
+	err = s.kv.Set(ctx, prefixRefreshToken(key), tk, 0)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *RefreshTokenStore) Delete(ctx context.Context, key string) error {
+	return errors.New("not implemented")
+}
