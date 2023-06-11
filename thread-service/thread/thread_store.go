57c3b03496c14cdc25f1b69f860be9f297f4e469
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2023-06-11 23:54:08 +0200

    chore: use ThreadModel as database access object

--- thread-service/thread/thread_store.go
+++ thread-service/thread/thread_store.go
@@ -15,7 +15,7 @@ type Store struct {
 }
 
 func (s *Store) Create(ctx context.Context, t *Thread) error {
-	err := s.DB.Create(ctx, t)
+	err := s.DB.Create(ctx, ThreadModelFromThread(t))
 	if err != nil {
 		return fmt.Errorf("unable to create thread: %w", err)
 	}
@@ -28,25 +28,25 @@ func (s *Store) Create(ctx context.Context, t *Thread) error {
 }
 
 func (s *Store) GetByID(ctx context.Context, id string) (*Thread, error) {
-	t := &Thread{}
+	t := &ThreadModel{}
 	err := s.DB.Find(t).Where("id = ?", id).Commit(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("unable to find thread by ID %q: %w", id, err)
 	}
-	return t, nil
+	return ThreadFromThreadModel(t), nil
 }
 
 func (s *Store) List(ctx context.Context) ([]*Thread, error) {
-	var threads []*Thread
+	var threads []*ThreadModel
 	err := s.DB.Find(&threads).Commit(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("unable to list threads: %w", err)
 	}
-	return threads, nil
+	return ThreadsFromThreadModels(threads), nil
 }
 
 func (s *Store) UpdateByID(ctx context.Context, t *Thread) error {
-	err := s.DB.Update(t).Where("id = ?", t.ID).Commit(ctx)
+	err := s.DB.Update(ThreadModelFromThread(t)).Where("id = ?", t.ID).Commit(ctx)
 	if err != nil {
 		return fmt.Errorf("unable to update thread: %w", err)
 	}
@@ -59,7 +59,7 @@ func (s *Store) UpdateByID(ctx context.Context, t *Thread) error {
 }
 
 func (s *Store) DeleteByID(ctx context.Context, id string) error {
-	err := s.DB.Delete(&Thread{}).Where("id = ?", id).Commit(ctx)
+	err := s.DB.Delete(&ThreadModel{}).Where("id = ?", id).Commit(ctx)
 	if err != nil {
 		return fmt.Errorf("unable to delete thread: %w", err)
 	}
