cfccbc44f8ab7f4a3b5e998a9429ffb1ab125c2e
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2023-06-12 00:22:05 +0200

    feat: added swagger api documentation

--- go.mod
+++ go.mod
@@ -15,22 +15,35 @@ require (
 )
 
 require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-openapi/jsonpointer v0.19.6 // indirect
+	github.com/go-openapi/jsonreference v0.20.2 // indirect
+	github.com/go-openapi/spec v0.20.9 // indirect
+	github.com/go-openapi/swag v0.22.4 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.3.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/compress v1.16.5 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/nats-io/nats-server/v2 v2.9.17 // indirect
 	github.com/nats-io/nkeys v0.4.4 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/swaggo/files/v2 v2.0.0 // indirect
+	github.com/swaggo/http-swagger/v2 v2.0.1 // indirect
+	github.com/swaggo/swag v1.16.1 // indirect
 	golang.org/x/crypto v0.8.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/tools v0.9.3 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- thread-service/main.go
+++ thread-service/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/leonsteinhaeuser/example-app/internal/db"
@@ -9,9 +11,15 @@ import (
 	"github.com/leonsteinhaeuser/example-app/internal/log"
 	"github.com/leonsteinhaeuser/example-app/internal/pubsub"
 	"github.com/leonsteinhaeuser/example-app/internal/server"
+	"github.com/leonsteinhaeuser/example-app/internal/utils"
 	v1 "github.com/leonsteinhaeuser/example-app/thread-service/api/v1"
+	"github.com/leonsteinhaeuser/example-app/thread-service/docs"
 	"github.com/leonsteinhaeuser/example-app/thread-service/thread"
 	"github.com/rs/zerolog"
+
+	httpSwagger "github.com/swaggo/http-swagger/v2"
+
+	_ "github.com/leonsteinhaeuser/example-app/thread-service/docs"
 )
 
 const (
@@ -20,7 +28,9 @@ const (
 
 var (
 	// server settings
-	listenAddr string = env.GetStringEnvOrDefault("LISTEN_ADDR", ":8080")
+	listenAddr string = env.GetStringEnvOrDefault("LISTEN_ADDR", ":1112")
+	// externalURL
+	externalURL string = env.GetStringEnvOrDefault("EXTERNAL_URL", "http://localhost:1112")
 	// logger settings
 	logLevel            = env.GetIntEnvOrDefault("LOG_LEVEL", 0)
 	logr     log.Logger = log.NewZerlog(func(l *zerolog.Logger) {
@@ -49,12 +59,25 @@ func init() {
 	ctx, cf := context.WithTimeout(context.Background(), time.Second*5)
 	defer cf()
 
-	err = dbr.Migrate(ctx, &thread.Thread{})
+	err = dbr.Migrate(ctx, &thread.ThreadModel{})
 	if err != nil {
 		logr.Panic(err).Log("failed to migrate database")
 	}
 }
 
+// @title Thread Service API
+// @version 1.0
+// @description This is the thread-service.
+
+// @contact.name API Support
+// @contact.url http://github.com/leonsteinhaeuser/example-app
+// @contact.email mail@example.localhost
+
+// @license.name GNU GENERAL PUBLIC LICENSE 3.0
+// @license.url https://github.com/leonsteinhaeuser/example-app/blob/main/LICENSE
+
+// @host thread-service
+// @BasePath /
 func main() {
 	srvr := server.NewDefaultServer(logr, listenAddr)
 
@@ -62,6 +85,15 @@ func main() {
 		DB: dbr,
 		PS: pubsubClient,
 	}))
+
+	// override swagger docs host
+	docs.SwaggerInfo.Host = utils.ReplaceAllToReplace(externalURL, "", "http://", "https://")
+	// define swagger endpoint to serve swagger docs
+	dfltrt := server.NewGenericRouter()
+	dfltrt.AddEndpoint(http.MethodGet, "/swagger/*", httpSwagger.Handler(
+		httpSwagger.URL(fmt.Sprintf("%s/swagger/doc.json", externalURL)),
+	))
+	srvr.AddRouter(dfltrt)
 
 	defer srvr.Stop()
 	logr.Info().Logf("starting server on %s", listenAddr)
