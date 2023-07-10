1291cc7314b170c06fc94c210a76939d949f964e
Leon Steinhäuser <leonsteinhaeuser@gmail.com>
2023-07-10 20:00:08 +0200

    chore: added missing code documentation

--- internal/utils/json.go
+++ internal/utils/json.go
@@ -5,12 +5,14 @@ import (
 	"net/http"
 )
 
+// WriteJSON writes the JSON representation of v to w.
 func WriteJSON(w http.ResponseWriter, statusCode int, v interface{}) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 	return json.NewEncoder(w).Encode(v)
 }
 
+// ReadJSON reads the JSON representation of v from r.
 func ReadJSON(r *http.Request, v interface{}) error {
 	defer r.Body.Close()
 	return json.NewDecoder(r.Body).Decode(v)
