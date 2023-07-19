f95afec3f7067df5ae85de2a484149080f15cb2e
Leon Steinhäuser <leonsteinhaeuser@gmail.com>
2023-07-19 22:58:17 +0200

    feat: use service provided client

--- view-service/main.go
+++ view-service/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
 	"html/template"
 	"net/http"
 
 	"github.com/leonsteinhaeuser/example-app/internal/env"
 	"github.com/leonsteinhaeuser/example-app/internal/log"
 	"github.com/leonsteinhaeuser/example-app/internal/server"
-	v1 "github.com/leonsteinhaeuser/example-app/number-service/api/v1"
+	"github.com/leonsteinhaeuser/example-app/number-service/client"
 )
 
 var (
@@ -32,6 +30,8 @@ var (
 	`
 
 	indexTemplate = template.New("index.html")
+
+	numberClient *client.RestClient
 )
 
 func init() {
@@ -40,18 +40,20 @@ func init() {
 		panic("failed to parse index.html template")
 	}
 	indexTemplate = idxTpl
+
+	numberClient = client.NewRestClient(logr, 5, numberServiceAddress)
 }
 
 func main() {
 	httpRouter.AddEndpoint("GET", "/", func(w http.ResponseWriter, r *http.Request) {
-		number, err := getNumberFromNumberService(r.Context())
+		number, err := numberClient.GetNumber(r.Context())
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
 		err = indexTemplate.Execute(w, map[string]interface{}{
-			"number": number,
+			"number": number.Number,
 		})
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -67,25 +69,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func getNumberFromNumberService(ctx context.Context) (int64, error) {
-	req, err := http.NewRequest("GET", numberServiceAddress, nil)
-	if err != nil {
-		return 0, err
-	}
-	// add context to request
-	req = req.WithContext(ctx)
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return 0, err
-	}
-	// parse response
-	data := v1.NumberResponse{}
-	err = json.NewDecoder(resp.Body).Decode(&data)
-	if err != nil {
-		return 0, err
-	}
-	return data.Number, nil
 }
